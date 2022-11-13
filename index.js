c1728016d12bf3fcb28316954decfc38e32dd566
jeichel09 <61138020+jeichel09@users.noreply.github.com>
2022-11-13 13:45:04 +0100

    Add files via upload (#1)

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-// Import stylesheets
-import './style.css';
+const express = require('express');
+const expressConfig = require('./config/express');
+const databaseConfig = require('./config/database');
+const routesConfig = require('./config/routes');
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+
+
+start();
+
+async function start(){
+    const app = express();
+
+    expressConfig(app);
+    await databaseConfig(app);
+    routesConfig(app);
+
+    app.listen(3000, () => console.log('Server listening on port 3000'));
+}
